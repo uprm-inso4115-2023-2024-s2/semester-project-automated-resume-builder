796624c7f33acf5b557a2d70c3915a677531fd10
JeremySanchez1 <jeremy.sanchez1@upr.edu>
2024-02-18 22:16:02 -0400

    Added routes for sign up and log in form pages

--- ui/client/src/App.js
+++ ui/client/src/App.js
@@ -5,6 +5,8 @@ import TaskForm from './components/TaskForm'
 import ResumeForm from './components/resume'
 import {Container} from '@mui/material'
 import Menu from "./components/NavBar"
+import SignUpForm from './components/SignUpForm.js';
+import LogInForm from './components/LogInForm.js';
 
 export default function App(){
   return (
@@ -17,6 +19,8 @@ export default function App(){
             <Route path='/tasks/new' element={<TaskForm />} />
             <Route path='/tasks/:id/edit' element={<TaskForm />} />
             <Route path='/resume/new' element={<ResumeForm />} />
+            <Route path='/signup' element={<SignUpForm onSignUp={() => {console.log("signed up")}} />} />
+            <Route path='/login' element={<LogInForm onLogIn={() => {console.log("Logged in")}} />} />
           </Routes>
         </Container>
       </BrowserRouter>
