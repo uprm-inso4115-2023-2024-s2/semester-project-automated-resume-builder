e0209fe0ca7075ebcc304e38ff00d80261c7d0e5
gabriel.padilla7 <gabriel.padilla7@upr.edu>
2024-02-21 18:32:01 -0400

    Added input verification for sign up form.

--- ui/client/src/components/SignUpForm.js
+++ ui/client/src/components/SignUpForm.js
@@ -13,8 +13,47 @@ const SignUpForm  = (onSignUp) => {
     agreeTerms: false,
   });
 
+  const [error, setError] = useState({
+    firstName: '',
+    lastName: '',
+    phone: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
+
+    let error = '';
+
+    // Validation 
+
+    switch (name) {
+      case 'firstName':
+        error = value.trim() === '' ? 'First name is required' : /\d/.test(value) ? 'First name should not contain numbers' : '';
+        break;
+      case 'lastName':
+        error = value.trim() === '' ? 'Last name is required' : /\d/.test(value) ? 'Last name should not contain numbers' : '';
+        break;
+      case 'phone':
+        error = value.trim() === '' ? 'Phone number is required' : /[a-zA-Z]/.test(value) ? 'Phone number should not contain letters' : '';
+        break;
+      case 'email':
+        error = value.trim() === '' ? 'Email is required' : !/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(value) ? 'Email is invalid' : '';
+        break;
+      case 'password':
+        error = value.trim() === '' ? 'Password is required' : !/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}/.test(value) ? 'Password is invalid, please make sure it has at least: uppercase letters, lowercase letters and numbers' : '';
+        break;
+      case 'confirmPassword':
+        error = value.trim() === '' ? 'Confirm password is required' : value !== formData.password ? 'Passwords do not match' : '';
+        break;
+      default:
+        break;
+    }
+
+    setError({...error, [name]: error});
+
     setFormData({
       ...formData,
       [name]: type === 'checkbox' ? checked : value,
@@ -24,6 +63,37 @@ const SignUpForm  = (onSignUp) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(formData);
+
+    for (let key in formData) {
+      let error = '';
+      switch(key){
+        case 'firstName':
+          error = formData[key].trim() === '' ? 'First name is required' : /\d/.test(formData[key]) ? 'First name should not contain numbers' : '';
+          break;
+        case 'lastName':
+          error = formData[key].trim() === '' ? 'Last name is required' : /\d/.test(formData[key]) ? 'Last name should not contain numbers' : '';
+          break;
+        case 'phone':
+          error = formData[key].trim() === '' ? 'Phone number is required' : /[a-zA-Z]/.test(formData[key]) ? 'Phone number should not contain letters' : '';
+          break;
+        case 'email':
+          error = formData[key].trim() === '' ? 'Email is required' : !/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(formData[key]) ? 'Email is invalid' : '';
+          break;
+        case 'password':
+          error = formData[key].trim() === '' ? 'Password is required' : !/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}/.test(formData[key]) ? 'Password is invalid, please make sure it has at least: uppercase letters, lowercase Letters and Numbers' : '';
+          break;
+        case 'confirmPassword':
+          error = formData[key].trim() === '' ? 'Confirm password is required' : formData[key] !== formData.password ? 'Passwords do not match' : '';
+          break;
+        default:
+          break;    
+      }
+      setError({...error, [key]: error});
+      if (error) return;
+    }
+
+    console.log('Form submitted')
+
     setFormData({
       firstName: '',
       lastName: '',
@@ -60,6 +130,8 @@ const SignUpForm  = (onSignUp) => {
             required
             fullWidth
             margin="normal"
+            error={!!error.firstName}
+            helperText={error.firstName}
             InputLabelProps={{ style: { color: 'white' } }}
             inputProps={{ style: { color: 'white' } }}
           />
@@ -74,6 +146,8 @@ const SignUpForm  = (onSignUp) => {
             required
             fullWidth
             margin="normal"
+            error={!!error.lastName}
+            helperText={error.lastName}
             InputLabelProps={{ style: { color: 'white' } }}
             inputProps={{ style: { color: 'white' } }}
           />
@@ -88,6 +162,8 @@ const SignUpForm  = (onSignUp) => {
             required
             fullWidth
             margin="normal"
+            error={!!error.phone}
+            helperText={error.phone}
             InputLabelProps={{ style: { color: 'white' } }}
             inputProps={{ style: { color: 'white' } }}
           />
@@ -102,6 +178,8 @@ const SignUpForm  = (onSignUp) => {
             required
             fullWidth
             margin="normal"
+            error={!!error.email}
+            helperText={error.email}
             InputLabelProps={{ style: { color: 'white' } }}
             inputProps={{ style: { color: 'white' } }}
           />
@@ -117,6 +195,8 @@ const SignUpForm  = (onSignUp) => {
             required
             fullWidth
             margin="normal"
+            error={!!error.password}
+            helperText={error.password}
             InputLabelProps={{ style: { color: 'white' } }}
             inputProps={{ style: { color: 'white' } }}
           />
@@ -132,6 +212,8 @@ const SignUpForm  = (onSignUp) => {
             required
             fullWidth
             margin="normal"
+            error={!!error.confirmPassword}
+            helperText={error.confirmPassword}
             InputLabelProps={{ style: { color: 'white' } }}
             inputProps={{ style: { color: 'white' } }}
           />
