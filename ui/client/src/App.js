1b363fb506a23d2f0d3784a6072c3ee66a198166
gabrielpadilla7 <gabriel.padilla7@upr.edu>
2024-02-27 18:48:14 -0400

    Basic Landing Page completed

--- ui/client/src/App.js
+++ ui/client/src/App.js
@@ -1,31 +1,30 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+// App.js
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { UserProvider } from './contexts/UserContext.js';
-import UserList from './components/UserList.js'
-import UserForm from './components/UserForm.js'
-import ResumeForm from './components/resume'
-import {Container} from '@mui/material'
-import Menu from "./components/NavBar"
+import { Container } from '@mui/material';
+import MenuContainer from "./components/MenuContainer";
+import LandingPage from './components/ResumeModals/LandingPage.js';
+import UserForm from './components/UserForm.js';
+import ResumeForm from './components/resume';
 import SignUpForm from './components/SignUpForm.js';
 import LogInForm from './components/LogInForm.js';
 import ResumeInput from './components/ResumeInput.js';
-import LandingPage from './components/ResumeModals/LandingPage.js';
 
-export default function App(){
+export default function App() {
   return (
     <UserProvider>
       <BrowserRouter>
-        <Menu />
+        <MenuContainer /> {/* Render the MenuContainer component */}
         <Container>
           <Routes>
-            {/*Landing Page*/}
+            {/* Landing Page */}
             <Route path='/' element={<LandingPage />} />
-            <Route path='/' element={<UserList />} />
             <Route path='/users/new' element={<UserForm />} />
             <Route path='/users/:id/edit' element={<UserForm />} />
             <Route path='/resume/new' element={<ResumeForm />} />
-            <Route path='/signup' element={<SignUpForm onSignUp={() => {console.log("signed up")}} />} />
-            <Route path='/login' element={<LogInForm onLogIn={() => {console.log("Logged in")}} />} />
-            <Route path='/resume/datainput' element={<ResumeInput onSubmitResumeForm={() => {console.log("Submitted resume info")}} />} />
+            <Route path='/signup' element={<SignUpForm onSignUp={() => { console.log("signed up") }} />} />
+            <Route path='/login' element={<LogInForm onLogIn={() => { console.log("Logged in") }} />} />
+            <Route path='/resume/datainput' element={<ResumeInput onSubmitResumeForm={() => { console.log("Submitted resume info") }} />} />
           </Routes>
         </Container>
       </BrowserRouter>
