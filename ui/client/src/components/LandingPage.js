56392daa29056c28a9bacad0ba1c2832c67484da
gabriel.padilla7 <gabriel.padilla7@upr.edu>
2024-03-04 13:45:13 -0400

    Separated the styling to optimize and make the code more readable

--- ui/client/src/components/LandingPage.js
+++ ui/client/src/components/LandingPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './LandingPage.css';
 
 function LandingPage() {
     const [started, setStarted] = useState(false);
@@ -9,14 +10,14 @@ function LandingPage() {
     }
 
     return (
-        <div style={styles.container}>
+        <div className='container'> {/* Apply custom styles to the container */}
             <h1>Welcome to the Resume Builder</h1>
             {!started && (
                 <div>
                     <p>Get started by creating a new resume</p>
                     {/* Apply custom styles to the Link */}
-                    <Link to="/resume/new" onClick={handleGetStarted} style={{ textDecoration: 'none', color: 'inherit' }}>
-                        <button style={{ textDecoration: 'none', color: 'black' }}>Start your resume!</button>
+                    <Link to="/resume/new" onClick={handleGetStarted} className='link'>
+                        <button className='button'>Start your resume!</button>
                     </Link>
                 </div>
             )}
@@ -27,22 +28,6 @@ function LandingPage() {
     );
 }
 
-// Define styles
-const styles = {
-    container: {
-        background: 'linear-gradient(to bottom, #7be495, #28a745)', // Brighter green variant with faster color transition
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: '20px',
-        color: 'white', // Text color
-    },
-};
+
 
 export default LandingPage;
