a9c218f0a19fcd32cc97ca00ad2c4ba506da9e35
BryanDVG <70717360+BryanDVG@users.noreply.github.com>
2024-04-20 22:25:08 -0400

    Implemented a ' Help' button in the NavBar

--- ui/client/src/components/LandingPage.js
+++ ui/client/src/components/LandingPage.js
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button } from '@mui/material';
 import './LandingPage.css';
 
 function LandingPage() {
     const [started, setStarted] = useState(false);
-
+    const [open, setOpen] = useState(false);
     const handleGetStarted = () => {
         setStarted(true);
     }
+    const handleClickOpen = () => {
+        setOpen(true); // Handler to open the dialog
+    };
+
+    const handleClose = () => {
+        setOpen(false); // Handler to close the dialog
+    };
 
     return (
         <div className='container'> {/* Apply custom styles to the container */}
@@ -20,6 +28,7 @@ function LandingPage() {
                 <Link to='/resume/datainput' className='link'>
                     <button className='top-button'>Resume</button>
                 </Link>
+                <button onClick={handleClickOpen} className='top-button'>Help</button>
             </div>
             <Link to="/profile" className='link'>
                 <div className='profile-picture'></div>
@@ -45,6 +54,27 @@ function LandingPage() {
             {started && (
                 <p>You have already started!</p>
             )}
+            <Dialog open={open} onClose={handleClose} >
+                <DialogTitle>{"How to create a Resume?"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText style={{ color: 'black' }}>
+                        1. Click on the 'Templates' button.<br></br><br></br>
+                        2. Choose the template that you prefered and click on it. An example resume of the template
+                        will appear. If you are convinced with the template click on 'USE THIS TEMPLATE'.<br></br><br></br>
+                        3. Fill in your information into the corresponding fields and click 'SUBMIT RESUME' when you are done.
+                        Then a 'Preview' button will appear, click on it.<br></br><br></br>
+                        4. A preview of your resume will appear. If everything seems to be in the right place, you can download
+                        it into a pdf, save it into your profile or share it using a link.<br></br><br></br>
+                        If you need any assistance or have questions about the process,
+                        click on the 'Help' button or contact us directly.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Close
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     );
 }
